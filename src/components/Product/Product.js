7562c8bafd7c146afb57097202477410030d3a1e
webhero-nur <nurealom522@gmail.com>
2023-02-09 11:44:05 +0600

    files added from previous repository

---
+++ src/components/Product/Product.js
@@ -0,0 +1,27 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+const Product = ({ handleAddToCart, product }) => {
+    // const { handleAddToCart, product } = props;
+    const { name, img, price, seller, ratings, category } = product;
+    const altText = `${category} : ${name} by ${seller}`;
+    return (
+        <div className='product'>
+            <img src={img} alt={altText} />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings}</small></p>
+            </div>
+            <button onClick={() => handleAddToCart(product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+        // do not do this: cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        handleAddToCart={handleAddToCart}
+                        product={product}
+                    ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h4>Order summary</h4>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
