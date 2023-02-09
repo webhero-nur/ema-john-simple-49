fe551757e0afc8ef3cd19b0d752689106434ebf1
webhero-nur <nurealom522@gmail.com>
2023-02-09 21:01:34 +0600

    Handle quantity from storage to cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -18,11 +18,10 @@ const Shop = () => {
         const storedCart = getStoredCart();
         const savedCart = [];
         for (const id in storedCart) {
-            const addedProduct = products.find(product => product.id === id)
+            const addedProduct = products.find(product => product.id === id);
             if (addedProduct) {
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
-                console.log(addedProduct);
                 savedCart.push(addedProduct);
             }
         }
